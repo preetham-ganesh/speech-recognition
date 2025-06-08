bcadf061a5d6dc7488df35b00940258893239a53
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-08 19:29:17 -0400

    [#3] added tokenize_text() to Dataset class.
    Tokenizes text to convert into ids using trained tokenizer.

--- src/deep_speech_2/dataset.py
+++ src/deep_speech_2/dataset.py
@@ -6,7 +6,7 @@ import librosa
 
 from src.utils import check_directory_path_existence, load_text_file
 
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 
 class Dataset(object):
@@ -219,3 +219,20 @@ class Dataset(object):
         # Transposes: librosa spectrogram from (freq_bins, time_steps) -> (time_steps, freq_bins).
         log_spectrogram = log_spectrogram.T
         return log_spectrogram
+
+    def tokenize_text(self, text: str) -> List[int]:
+        """Tokenizes text to convert into ids using trained tokenizer.
+
+        Tokenizes text to convert into ids using trained tokenizer.
+
+        Args:
+            text: A string for the target text that should be tokenized.
+
+        Returns:
+            A list of integers for the tokenized & encoded version of the text.
+        """
+        # Checks types & values of arguments.
+        assert isinstance(text, str), "Variable text should be of type 'str'."
+
+        # Tokenizes characters into ids based on trained tokenizer.
+        return [self.char_to_id[c] for c in text]
