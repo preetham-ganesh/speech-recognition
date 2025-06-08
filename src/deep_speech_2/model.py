47a86a9992c0792204f86187c2a331f6eaf4a13b
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-07 23:21:25 -0400

    [#1] updated build_graph() in DeepSpeech2 class.
    Builds concreate model first & then flexible model for plottable graph.

--- src/deep_speech_2/model.py
+++ src/deep_speech_2/model.py
@@ -156,5 +156,12 @@ class DeepSpeech2(tf.keras.Model):
         Returns:
             A tensorflow keras model for the current model configuration.
         """
+        # Builds the model & all the layers with concrete inputs.
+        concrete_inputs = tf.keras.layers.Input(
+            shape=(200, 161, 1), name="input_concrete"
+        )
+        _ = self.call([concrete_inputs], False)
+
+        # Builds the model with flexible inputs.
         inputs = tf.keras.layers.Input(shape=(None, None, 1), name="input_0")
         return tf.keras.Model(inputs=inputs, outputs=self.call(inputs, False))
