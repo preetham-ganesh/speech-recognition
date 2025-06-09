93549d4195fc8de597b2f1ba944312be3dfe657c
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-09 09:29:36 -0400

    [#4] added download_dataset() to Dataset class.
    Downloads the LibriSpeech dataset using the OpenSLR links.

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,11 @@
+import os
+import time
+
+import requests
+from tqdm import tqdm
+
+from src.utils import check_directory_path_existence
+
 from typing import Dict, Any, List
 
 
@@ -27,3 +35,63 @@ class Dataset(object):
             "validation": {"file_path": list(), "text": list()},
             "test": {"file_path": list(), "text": list()},
         }
+
+    def download_dataset(self) -> None:
+        """Downloads the LibriSpeech dataset using the OpenSLR links.
+
+        Downloads the LibriSpeech dataset using the OpenSLR links.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # A dictionary for the data split based dataset links.
+        dataset_links = {
+            "test": "https://www.openslr.org/resources/12/test-clean.tar.gz",
+            "validation": "https://www.openslr.org/resources/12/dev-clean.tar.gz",
+            "train": "https://www.openslr.org/resources/12/train-clean-360.tar.gz",
+        }
+
+        # Checks if the following directory path exists.
+        dataset_directory_path = check_directory_path_existence(
+            os.path.join("data", "raw_data", "librispeech")
+        )
+
+        # Iterates across dataset links.
+        for file_name, link in dataset_links.items():
+            start_time = time.time()
+
+            # Checks if the file already exists. If yes, then does not download the file.
+            file_path = os.path.join(dataset_directory_path, f"{file_name}.tgz")
+            if os.path.exists(file_path):
+                print(f"{file_name}.tgz already exists.")
+                print()
+                continue
+
+            # Sends request for the current language dataset file.
+            response = requests.get(link, stream=True)
+
+            # Checks if the response has a success code. If not then prints the error message.
+            assert response.status_code == 200, response.text
+
+            # Gets total file size from headers (in bytes).
+            total_size = int(response.headers.get("content-length", 0))
+
+            # Downloads the file with progress bar.
+            with open(file_path, "wb") as out_file, tqdm(
+                desc=f"Downloading {file_name}.tgz",
+                total=total_size,
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as pbar:
+                for data in response.iter_content(chunk_size=1024):
+                    out_file.write(data)
+                    pbar.update(len(data))
+
+            print(
+                f"Finished downloading dataset for {file_name} split in {(time.time() - start_time):.3f} sec."
+            )
+            print()
