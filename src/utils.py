8b3d6168936bc80d32d36af37198a4de0e6e3f96
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-09 10:25:59 -0400

    [#4] added load_json_file().
    Loads a JSON file as a dictionary.

--- src/utils.py
+++ src/utils.py
@@ -62,3 +62,37 @@ def load_text_file(file_name: str, directory_path: str) -> str:
 
     except FileNotFoundError:
         raise FileNotFoundError(f"{file_path} does not exist.")
+
+
+def load_json_file(file_name: str, directory_path: str) -> Dict[Any, Any]:
+    """Loads a JSON file as a dictionary.
+
+    Loads a JSON file as a dictionary into memory based on the file_name.
+
+    Args:
+        file_name: A string for the name of the of the file to be loaded.
+        directory_path: A string for the location where the directory path exists.
+
+    Returns:
+        A dictionary loaded from the JSON file.
+
+    Exceptions:
+        FileNotFoundError: If file path does not exist, then this error occurs.
+    """
+    # Types checks input arguments.
+    assert isinstance(file_name, str), "Variable file_name should be of type 'str'."
+    assert isinstance(
+        directory_path, str
+    ), "Variable directory_path should be of type 'str'."
+
+    file_path = os.path.join(directory_path, f"{file_name}.json")
+
+    # Loads the JSON file as dictionary from the file location.
+    try:
+        with open(file_path, "r") as out_file:
+            dictionary = json.load(out_file)
+        out_file.close()
+        return dictionary
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File path {file_path} does not exist.")
