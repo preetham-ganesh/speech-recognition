db84cf1bf374635da0723b60565224ac64fb466d
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-09 16:42:47 -0400

    [#4] added compute_loss() to Train class.
    Computes loss for the current batch using actual & predicted values.

--- src/train.py
+++ src/train.py
@@ -198,3 +198,30 @@ class Train(object):
         self.validation_loss = tf.keras.metrics.Mean(name="validation_loss")
         self.train_cer = tf.keras.metrics.Mean(name="train_cer")
         self.validation_cer = tf.keras.metrics.Mean(name="validation_cer")
+
+    def compute_loss(
+        self, target_batch: tf.Tensor, predicted_batch: tf.Tensor
+    ) -> tf.Tensor:
+        """Computes loss for the current batch using actual & predicted values.
+
+        Computes loss for the current batch using actual & predicted values.
+
+        Args:
+            target_batch: A tensor for target batch of generated mask images.
+            predicted_batch: A tensor for batch of outputs predicted by the model for input batch.
+
+        Returns:
+            A tensor for the loss computed on comparing target & predicted batch.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(
+            target_batch, tf.Tensor
+        ), "Variable target_batch should be of type 'tf.Tensor'."
+        assert isinstance(
+            predicted_batch, tf.Tensor
+        ), "Variable predicted_batch should be of type 'tf.Tensor'."
+
+        # Computes loss for current target & predicted batches.
+        loss_object = tf.keras.losses.CTC(reduction="sum_over_batch_size", name="ctc")
+        current_loss = loss_object(target_batch, predicted_batch)
+        return current_loss
