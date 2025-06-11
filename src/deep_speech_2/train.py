77ac5592866b681335d17c164d90b26e466f6fb2
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-11 18:47:30 -0400

    [#4] added load_model_configuration() to Train class.
    Loads the model configuration file for model version.

--- src/deep_speech_2/train.py
+++ src/deep_speech_2/train.py
@@ -1,3 +1,10 @@
+import os
+
+import mlflow
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the DeepSpeech2 model based on the configuration."""
 
@@ -19,3 +26,34 @@ class Train(object):
         self.model_version = model_version
         self.best_validation_loss = None
         self.step = 0
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = os.path.join(
+            self.home_directory_path, "configs"
+        )
+        self.model_configuration = load_json_file(
+            f"v{self.model_version}", model_configuration_directory_path
+        )
+
+        # Sets tag in MLFlow.
+        mlflow.set_tag(
+            "architecture", self.model_configuration["model"]["architecture"]
+        )
+
+        # Logs parameters in MLFlow.
+        mlflow.log_param(
+            "conv_filters", self.model_configuration["model"]["conv_filters"]
+        )
+        mlflow.log_param("rnn_units", self.model_configuration["model"]["rnn_units"])
+        mlflow.log_param("rnn_blocks", self.model_configuration["model"]["rnn_blocks"])
