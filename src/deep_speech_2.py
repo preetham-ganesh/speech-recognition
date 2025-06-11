4ddd13fe14e5b03488e8697c1bbbc05d9745846c
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-11 18:31:25 -0400

    [#4] added call() to DeepSpeech2 class.
    Inputs are passed through the layers in the model.

--- src/deep_speech_2.py
+++ src/deep_speech_2.py
@@ -1,29 +1,18 @@
 import tensorflow as tf
 
-from typing import List
+from typing import Dict, Any, List
 
 
 class DeepSpeech2(tf.keras.Model):
     """A tensorflow model for recognizing text in speech using DeepSpeech2 architecture."""
 
-    def __init__(
-        self,
-        conv_filters: int,
-        rnn_units: int,
-        vocab_size: int,
-        rnn_blocks: int,
-        rate: float,
-    ) -> None:
+    def __init__(self, model_configuration: Dict[str, Any]) -> None:
         """Initializes the DeepSpeech2 model, by adding various layers.
 
         Initializes the DeepSpeech2 model, by adding various layers.
 
         Args:
-            conv_filters: An integer for the no. of filters in each Conv2D layer in the model.
-            rnn_units: An integer for the no. of units in each RNN layer.
-            vocab_size: An integer for the size of the vocabulary in the language.
-            rnn_blocks: An integer for the no. of RNN blocks in the model.
-            rate: A floating point value for the dropout rate in the model.
+            model_configuration: A dictionary for the configuration of the model.
 
         Returns:
             None.
@@ -32,22 +21,12 @@ class DeepSpeech2(tf.keras.Model):
 
         # Asserts type of input arguments.
         assert isinstance(
-            conv_filters, int
-        ), "Variable conv_filters should be of type 'int'."
-        assert isinstance(rnn_units, int), "Variable rnn_units should be of type 'int'."
-        assert isinstance(
-            vocab_size, int
-        ), "Variable vocab_size should be of type of 'int'."
-        assert (
-            isinstance(rnn_blocks, int) and rnn_blocks > 0
-        ), "Variable rnn_blocks should be of type 'int' and value greater than 0."
-        assert (
-            isinstance(rate, float) and 0 <= rate <= 1
-        ), "Variable rate should be of type 'float' and value between 0 & 1 (inclusive)."
+            model_configuration, dict
+        ), "Variable model_configuration should be of type 'dict'."
 
         # Initializes class variables.
-        self.cnn_blocks = 1
-        self.rnn_blocks = rnn_blocks
+        self.model_configuration = model_configuration
+        self.cnn_blocks = 2
         self.model_layers = dict()
 
         # Initializes a multiple Conv Blocks, each with Conv2D, BatchNormalization, & ReLU activation layers.
@@ -55,7 +34,7 @@ class DeepSpeech2(tf.keras.Model):
         stride_vals = [(2, 2), (1, 2)]
         for b_id in range(self.cnn_blocks):
             self.model_layers[f"block_{b_id}_conv2d_0"] = tf.keras.layers.Conv2D(
-                filters=conv_filters,
+                filters=model_configuration["model"]["conv_filters"],
                 kernel_size=kernel_sizes[b_id],
                 strides=stride_vals[b_id],
                 padding="same",
@@ -69,47 +48,46 @@ class DeepSpeech2(tf.keras.Model):
                 name=f"block_{b_id}_relu_0"
             )
 
-        # Initially a Reshape layer to reshape the output from Conv Blocks.
-        self.model_layers["reshape_0"] = tf.keras.layers.Lambda(
-            lambda t: tf.reshape(
-                t, [tf.shape(t)[0], tf.shape(t)[1], tf.shape(t)[2] * tf.shape(t)[3]]
+        # Initialies a Reshape layer to reshape the output from Conv Blocks.
+        self.model_layers["reshape_0"] = tf.keras.layers.Reshape(
+            target_shape=(
+                -1,
+                int(
+                    tf.math.ceil(self.model_configuration["model"]["n_mels"] / 4)
+                    * model_configuration["model"]["conv_filters"]
+                ),
             ),
             name="reshape_0",
         )
 
         # Initializes multiple Bidirectional LSTM blocks.
-        for b_id in range(self.rnn_blocks):
-            self.model_layers[f"block_{b_id}_rnn_fwd"] = tf.keras.layers.LSTM(
-                units=rnn_units,
-                return_state=True,
-                return_sequences=True,
-                name=f"block_{b_id}_rnn_fwd",
-            )
-            self.model_layers[f"block_{b_id}_rnn_bwd"] = tf.keras.layers.LSTM(
-                units=rnn_units,
-                return_state=True,
-                return_sequences=True,
-                go_backwards=True,
-                name=f"block_{b_id}_rnn_bwd",
-            )
+        for b_id in range(model_configuration["model"]["rnn_blocks"]):
             self.model_layers[f"block_{b_id}_bi_rnn"] = tf.keras.layers.Bidirectional(
-                layer=self.model_layers[f"block_{b_id}_rnn_fwd"],
-                backward_layer=self.model_layers[f"block_{b_id}_rnn_bwd"],
+                tf.keras.layers.LSTM(
+                    units=model_configuration["model"]["rnn_units"],
+                    return_sequences=True,
+                    name=f"block_{b_id}_rnn_fwd",
+                ),
                 merge_mode="concat",
                 name=f"block_{b_id}_bi_rnn",
             )
             self.model_layers[f"block_{b_id}_dropout_0"] = tf.keras.layers.Dropout(
-                rate=rate, name=f"block_{b_id}_dropout_0"
+                rate=model_configuration["model"]["rate"],
+                name=f"block_{b_id}_dropout_0",
             )
 
         # Initializes the final dense layers.
         self.model_layers["dense_0"] = tf.keras.layers.Dense(
-            units=rnn_units, activation="relu", name="dense_0"
+            units=model_configuration["model"]["rnn_units"],
+            activation="relu",
+            name="dense_0",
         )
         self.model_layers["dropout_0"] = tf.keras.layers.Dropout(
-            rate=rate, name="dropout_0"
+            rate=model_configuration["model"]["rate"], name="dropout_0"
         )
-        self.model_layers["final"] = tf.keras.layers.Dense(vocab_size, name="final")
+        self.model_layers["final"] = tf.keras.layers.Dense(
+            model_configuration["model"]["vocab_size"], name="final"
+        )
 
     def call(self, inputs: List[tf.Tensor], training: bool = False) -> List[tf.Tensor]:
         """Inputs are passed through the layers in the model.
@@ -130,13 +108,14 @@ class DeepSpeech2(tf.keras.Model):
             x = self.model_layers[f"block_{b_id}_conv2d_0"](x)
             x = self.model_layers[f"block_{b_id}_bn_0"](x)
             x = self.model_layers[f"block_{b_id}_relu_0"](x)
+            print(b_id, x.shape)
 
         # Passes the Conv block output through the Reshape layer.
         x = self.model_layers["reshape_0"](x)
 
         # Passes the Reshaped Conv block output (or RNN block output) through the RNN blocks.
-        for b_id in range(self.rnn_blocks):
-            x, *_ = self.model_layers[f"block_{b_id}_bi_rnn"](x)
+        for b_id in range(self.model_configuration["model"]["rnn_blocks"]):
+            x = self.model_layers[f"block_{b_id}_bi_rnn"](x)
             x = self.model_layers[f"block_{b_id}_dropout_0"](x, training=training)
 
         # Passes the RNN block output through the final layers.
@@ -145,23 +124,24 @@ class DeepSpeech2(tf.keras.Model):
         x = self.model_layers["final"](x)
         return x
 
-    def build_graph(self) -> tf.keras.Model:
+    def build_graph(self, max_spectrogram_length: int, n_mels: int) -> tf.keras.Model:
         """Builds plottable graph for the model.
 
         Builds plottable graph for the model.
 
         Args:
-            None.
+            max_spectrogram_length: An integer for the maximum no. of time frames in a spectrogram.
+            n_mels: An integer for the no. of mel frequency bins to compute.
 
         Returns:
             A tensorflow keras model for the current model configuration.
         """
-        # Builds the model & all the layers with concrete inputs.
-        concrete_inputs = tf.keras.layers.Input(
-            shape=(200, 161, 1), name="input_concrete"
+        inputs = tf.keras.layers.Input(
+            shape=(
+                self.model_configuration["model"]["max_spectrogram_length"],
+                self.model_configuration["model"]["n_mels"],
+                1,
+            ),
+            name="input_0",
         )
-        _ = self.call([concrete_inputs], False)
-
-        # Builds the model with flexible inputs.
-        inputs = tf.keras.layers.Input(shape=(None, None, 1), name="input_0")
-        return tf.keras.Model(inputs=inputs, outputs=self.call(inputs, False))
+        return tf.keras.Model(inputs=inputs, outputs=self.call([inputs], False))
