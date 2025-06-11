1507ac85ddef7ddfc65c0eb5eac4558a72c823fc
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-11 19:47:22 -0400

    [#4] added train_step() to Train class.
    Trains the model using input & target batches.

--- src/deep_speech_2/train.py
+++ src/deep_speech_2/train.py
@@ -231,3 +231,47 @@ class Train(object):
             logits_time_major=False,
         )
         return tf.reduce_mean(current_loss)
+
+    @tf.function
+    def train_step(
+        self,
+        input_batch: tf.Tensor,
+        target_batch: tf.Tensor,
+        target_lengths: tf.Tensor,
+    ) -> None:
+        """Trains the model using input & target batches.
+
+        Trains the model using input & target batches.
+
+        Args:
+            input_batch: A tensor for input batch of processed images.
+            target_batch: A tensor for target batch of generated mask images.
+            target_lengths: A tensor for the batch of target sequence length per sample (before padding).
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(
+            input_batch, tf.Tensor
+        ), "Variable input_batch should be of type 'tf.Tensor'."
+        assert isinstance(
+            target_batch, tf.Tensor
+        ), "Variable target_batch should be of type 'tf.Tensor'."
+        assert isinstance(
+            target_lengths, tf.Tensor
+        ), "Variable target_lengths should be of type 'tf.Tensor'."
+
+        # Computes predicted audio transcriptions for all audio files in the batch, and computes batch loss.
+        with tf.GradientTape() as tape:
+            predicted_batch = self.model([input_batch], training=True)
+            batch_loss = self.compute_loss(
+                target_batch, predicted_batch, target_lengths
+            )
+
+        # Computes gradients using loss. Apply the computed gradients on model variables using optimizer.
+        gradients = tape.gradient(batch_loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        # Computes mean for loss.
+        self.train_loss(batch_loss)
