f8ba4127b1c0e4586a4ee144d34623b9af7eaa5d
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-11 18:32:05 -0400

    [#4] added build_graph() to DeepSpeech2 class.
    Builds plottable graph for the model.

--- src/deep_speech_2/model.py
+++ src/deep_speech_2/model.py
@@ -88,3 +88,58 @@ class DeepSpeech2(tf.keras.Model):
         self.model_layers["final"] = tf.keras.layers.Dense(
             model_configuration["model"]["vocab_size"], name="final"
         )
+
+    def call(self, inputs: List[tf.Tensor], training: bool = False) -> List[tf.Tensor]:
+        """Inputs are passed through the layers in the model.
+
+        Inputs are passed through the layers in the model.
+
+        Args:
+            inputs: A list of input tensors from the input batch.
+            training: A boolean value for the flag of training/testing state.
+
+        Returns:
+            A tensors for the output predicted by the model for the current inputs.
+        """
+        x = inputs[0]
+
+        # Passes the inputs through the Conv blocks.
+        for b_id in range(self.cnn_blocks):
+            x = self.model_layers[f"block_{b_id}_conv2d_0"](x)
+            x = self.model_layers[f"block_{b_id}_bn_0"](x)
+            x = self.model_layers[f"block_{b_id}_relu_0"](x)
+
+        # Passes the Conv block output through the Reshape layer.
+        x = self.model_layers["reshape_0"](x)
+
+        # Passes the Reshaped Conv block output (or RNN block output) through the RNN blocks.
+        for b_id in range(self.model_configuration["model"]["rnn_blocks"]):
+            x = self.model_layers[f"block_{b_id}_bi_rnn"](x)
+            x = self.model_layers[f"block_{b_id}_dropout_0"](x, training=training)
+
+        # Passes the RNN block output through the final layers.
+        x = self.model_layers["dense_0"](x)
+        x = self.model_layers["dropout_0"](x, training=training)
+        x = self.model_layers["final"](x)
+        return x
+
+    def build_graph(self) -> tf.keras.Model:
+        """Builds plottable graph for the model.
+
+        Builds plottable graph for the model.
+
+        Args:
+            None.
+
+        Returns:
+            A tensorflow keras model for the current model configuration.
+        """
+        inputs = tf.keras.layers.Input(
+            shape=(
+                self.model_configuration["model"]["max_spectrogram_length"],
+                self.model_configuration["model"]["n_mels"],
+                1,
+            ),
+            name="input_0",
+        )
+        return tf.keras.Model(inputs=inputs, outputs=self.call([inputs], False))
