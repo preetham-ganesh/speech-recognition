3ea44ab30dc57a0699e74e057793ca39fab81ef3
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-11 20:59:17 -0400

    [#4] added main() to run.py.

---
+++ src/deep_speech_2/run.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import warnings
+import logging
+import argparse
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_PATH)
+warnings.filterwarnings("ignore")
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
+
+
+import mlflow
+
+from src.utils import set_physical_devices_memory_limit, check_directory_path_existence
+from src.deep_speech_2.train import Train
+
+
+def main():
+    print()
+
+    # Parses the arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-en",
+        "--experiment_name",
+        type=str,
+        required=True,
+        help="Name of the MLFlow experiment on which the model should be trained.",
+    )
+    parser.add_argument(
+        "-mv",
+        "--model_version",
+        type=str,
+        required=True,
+        help="Version by which the trained model files should be saved as.",
+    )
+    args = parser.parse_args()
+
+    # Sets memory limit of GPU if found in the system.
+    set_physical_devices_memory_limit()
+
+    # Creates the directory path.
+    mlruns_directory_path = check_directory_path_existence("mlruns")
+
+    # Sets tracking URI for MLFlow server.
+    mlflow.set_tracking_uri(mlruns_directory_path)
+
+    # Gets experiment based on name.
+    existing_experiment = mlflow.get_experiment_by_name(args.experiment_name)
+
+    # If experiment does not exist, then creates experiment with name & set it.
+    if not existing_experiment:
+        mlflow.create_experiment(args.experiment_name, artifact_location="")
+    mlflow.set_experiment(args.experiment_name)
+
+    # Sets tag for model version.
+    mlflow.set_tag("model_version", "v{}".format(args.model_version))
+
+    # Creates an object for the Train class.
+    trainer = Train(args.model_version)
+
+    # Loads model configuration for current model version.
+    trainer.load_model_configuration()
+
+    # Loads dataset based on dataset version in the model configuration.
+    trainer.load_dataset()
+
+    # Loads model & other utilies based on model configuration.
+    trainer.load_model()
+
+    # Generates summary & plot for loaded model.
+    trainer.generate_model_summary_and_plot(True)
+
+    # Trains & validates the model using train & validation dataset.
+    trainer.fit()
+
+    # Tests the model using the test dataset.
+    trainer.test_model()
+
+    # Serializes model as TensorFlow module & saves it as MLFlow artifact.
+    trainer.serialize_model()
+
+
+if __name__ == "__main__":
+    main()
