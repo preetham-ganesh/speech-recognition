3308f39ed9b4b64ed579586666cd2e99d3b735b4
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-16 12:33:09 -0400

    [#14] added load_dataset_info() to Dataset class.
    Loads file paths and transcription texts for the LibriSpeech dataset.

--- src/transformer/dataset.py
+++ src/transformer/dataset.py
@@ -1,3 +1,9 @@
+import os
+
+import pandas as pd
+
+from src.utils import check_directory_path_existence
+
 from typing import Dict, Any, List
 
 
@@ -20,3 +26,52 @@ class Dataset(object):
 
         # Initalizes class variables.
         self.model_configuration = model_configuration
+
+    def load_dataset_info(self) -> None:
+        """Loads file paths and transcription texts for the LibriSpeech dataset.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Creates an empty dictionary to store dataset info.
+        self.dataset_info = dict()
+
+        # Checks if the following directory path exists.
+        processed_data_directory_path = check_directory_path_existence(
+            os.path.join(
+                "data",
+                "processed_data",
+                "librispeech",
+                f"v{self.model_configuration['dataset']['version']}",
+            )
+        )
+
+        # Loads the processed dataset info for train split.
+        self.dataset_info["train"] = pd.read_csv(
+            os.path.join(processed_data_directory_path, "train", "dataset_info.csv")
+        )
+        print(
+            f"No. of examples in the train data split: {len(self.dataset_info['train'])}"
+        )
+
+        # Loads the processed dataset info for validation split.
+        self.dataset_info["validation"] = pd.read_csv(
+            os.path.join(
+                processed_data_directory_path, "validation", "dataset_info.csv"
+            )
+        )
+        print(
+            f"No. of examples in the validation data split: {len(self.dataset_info['validation'])}"
+        )
+
+        # Loads the processed dataset info for test split.
+        self.dataset_info["test"] = pd.read_csv(
+            os.path.join(processed_data_directory_path, "test", "dataset_info.csv")
+        )
+        print(
+            f"No. of examples in the test data split: {len(self.dataset_info['test'])}"
+        )
+        print()
