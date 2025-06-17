82e59a736e1076f5a3a31a19031b3c73c871c06a
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-17 18:50:11 -0400

    [#5] added load_model_configuration() to SpeechRecognition class.
    Loads the model configuration file for model version.

--- src/transformer/predict.py
+++ src/transformer/predict.py
@@ -12,6 +12,9 @@ warnings.filterwarnings("ignore")
 logging.getLogger("tensorflow").setLevel(logging.FATAL)
 
 
+from src.utils import load_json_file
+
+
 class SpeechRecognition(object):
     """"""
 
@@ -31,3 +34,22 @@ class SpeechRecognition(object):
 
         # Initalizes class variables.
         self.model_version = model_version
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = os.path.join(
+            self.home_directory_path, "models", "transformer", f"v{self.model_version}"
+        )
+        self.model_configuration = load_json_file(
+            "model_configuration", model_configuration_directory_path
+        )
