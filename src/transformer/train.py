d4020e2bbf67149861d880d458002c3a19038130
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-17 11:44:53 -0400

    [#6] added initialize_metric_trackers() to Train class.
    Initializes trackers which computes the mean of all metrics.

--- src/transformer/train.py
+++ src/transformer/train.py
@@ -221,3 +221,17 @@ class Train(object):
                 os.path.join(self.reports_directory_path, "model_plot.png"),
                 f"v{self.model_version}",
             )
+
+    def initialize_metric_trackers(self) -> None:
+        """Initializes trackers which computes the mean of all metrics.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.train_loss = tf.keras.metrics.Mean(name="train_loss")
+        self.validation_loss = tf.keras.metrics.Mean(name="validation_loss")
+        self.train_accuracy = tf.keras.metrics.Mean(name="train_accuracy")
+        self.validation_accuracy = tf.keras.metrics.Mean(name="validation_accuracy")
