8131f8b24ff94b13454d2678704a6cf49a94aac9
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2025-06-17 10:46:58 -0400

    [#6] added load_model_configuration() to Train class.
    Loads the model configuration file for model version.

--- src/transformer/train.py
+++ src/transformer/train.py
@@ -1,3 +1,10 @@
+import os
+
+import mlflow
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the ASR Transformer model based on the configuration."""
 
@@ -16,3 +23,36 @@ class Train(object):
         # Initalizes class variables.
         self.model_version = model_version
         self.best_validation_loss = None
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for model version.
+
+        Loads the model configuration file for model version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = os.path.join(
+            self.home_directory_path, "configs", "transformer"
+        )
+        self.model_configuration = load_json_file(
+            f"v{self.model_version}", model_configuration_directory_path
+        )
+
+        # Sets tag in MLFlow.
+        mlflow.set_tag(
+            "architecture", self.model_configuration["model"]["architecture"]
+        )
+        mlflow.set_tag("dataset_size", self.model_configuration["dataset"]["size"])
+        mlflow.set_tag(
+            "dataset_version", self.model_configuration["dataset"]["version"]
+        )
+        mlflow.set_tag("model_type", self.model_configuration["model"]["type"])
+
+        # Logs parameters in MLFlow.
+        mlflow.log_param("n_layers", self.n_layers)
+        mlflow.log_param("d_units", self.d_units)
